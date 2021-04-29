d665708bdf96f620416346e4a1acd3257fa2e1b3
Davi Chaves <dchaves@usc.edu>
2021-04-28 23:58:34 -0300

    running migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_04_28_024630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "currencies", force: :cascade do |t|
+    t.string "name"
+    t.string "symbol"
+    t.string "country"
+    t.string "flag"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "exchange_rates", force: :cascade do |t|
+    t.integer "from_currency_id"
+    t.integer "to_currency_id"
+    t.decimal "rate"
+    t.datetime "at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
